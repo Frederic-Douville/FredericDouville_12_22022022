86ef5448254e456007a222cfa59f2a5068c4641c
Frederic Douville <frederic.douville@zoho.eu>
2022-02-25 17:30:29 +0100

    initialisation du projet avec create react app

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+import './header.css';
+
+function Header() {
+    return (
+        <div className="header-ctn">
+            <Link to="/">
+                <img src={Logo} alt="logo" className="logo" />
+            </Link>
+            <nav>
+                <Link to="/" className="header-link">
+                    Accueil
+                </Link>
+                <Link to="/profil/:id" className="header-link">
+                    Profil
+                </Link>
+                <Link to="/" className="header-link">
+                    Réglage
+                </Link>
+                <Link to="/" className="header-link">
+                    Communauté
+                </Link>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
